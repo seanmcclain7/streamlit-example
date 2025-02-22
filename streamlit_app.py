ea07b5d87895b3d2cd3d507c93de30b5851c0a4b
Sean Mcclain <sean.mcclain@lakefrontdatasolutions.com>
2025-02-22 15:30:53 -0500

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,11 @@
+import os
 import altair as alt
 import numpy as np
 import pandas as pd
 import streamlit as st
+
+# Get the port from the environment variable
+port = int(os.environ.get("PORT", 8501))
 
 num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
 num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
